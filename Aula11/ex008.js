214628dd089d930514418d4604711ab9f6df6e46
Lucas Gonzaga <lucassoft@gmx.com>
2023-05-02 11:17:37 -0300

    aula 11+  exerc.

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velecidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ // CONDIÇÃO SIMPLES
+    console.log(`Você utrapassol a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando sinto de seguraça!`)
